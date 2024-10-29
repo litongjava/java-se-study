a8ef31fcdd1f831675c746f3371cf57ba354e41a
Tong Li <litongjava@qq.com>
2024-10-29 10:54:54 -1000

    finish assignment 15

--- maven/java-ics111/src/main/java/Assignment15LiTongMenu.java
+++ maven/java-ics111/src/main/java/Assignment15LiTongMenu.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /**
  * Assignment 15 Menu.
  * 
- * This program generates a list of random phone numbers based on user input.
- * It allows users to set the number of phone numbers to generate, print the list,
+ * This program generates a list of random phone numbers based on user input. It
+ * allows users to set the number of phone numbers to generate, print the list,
  * or exit the program.
  * 
  * @author Tong Li
@@ -15,92 +15,94 @@ import java.util.Scanner;
  */
 public class Assignment15LiTongMenu {
 
-  /**
-   * Main method to run the program.
-   * 
-   * @param args command-line arguments
-   */
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    boolean keepGoing = true;
-    int limit = 100;
+	/**
+	 * Main method to run the program.
+	 * 
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean keepGoing = true;
+		int limit = 100;
 
-    while (keepGoing) {
-      System.out.println("Please enter the number of your choice:");
-      System.out.println("1 to change the amount of phone numbers to generate (defaults to 100)");
-      System.out.println("2 to print a list of phone numbers");
-      System.out.println("0 to exit");
-      System.out.println("======================================");
+		while (keepGoing) {
+			System.out.println("Please enter the number of your choice:");
+			System.out.println("1 to change the amount of phone numbers to generate"
+					+ " (defaults to 100)");
+			System.out.println("2 to print a list of phone numbers");
+			System.out.println("0 to exit");
+			System.out.println("======================================");
 
-      String temp = scanner.nextLine();
-      switch (temp) {
-      case "0": {
-        System.out.println("You have chosen to exit");
-        keepGoing = false;
-        break;
-      }
-      case "1": {
-        System.out.println("Please enter the amount of phone numbers you want:");
-        temp = scanner.nextLine();
-        boolean flag = true;
-        try {
-          limit = Integer.parseInt(temp);
-        } catch (NumberFormatException e) {
-          flag = false;
-        }
+			String temp = scanner.nextLine();
+			switch (temp) {
+			   case "0": 
+				   System.out.println("You have chosen to exit");
+				   keepGoing = false;
+				   break;
+			
+			   case "1": 
+				   System.out.println("Please enter the amount of phone numbers you want:");
+				   temp = scanner.nextLine();
+				   boolean flag = true;
+				   try {
+					   limit = Integer.parseInt(temp);
+				   } catch (NumberFormatException e) {
+					   flag = false;
+				   }
 
-        if (flag) {
-          System.out.println("The amount of numbers to generate has been changed to " + limit);
-        } else {
-          System.out.println("You did not give me an integer number.");
-          System.out.println("The amount of numbers will stay at: " + limit);
-          System.out.println();
-          System.out.println("======================================");
-        }
-        break;
-      }
-      case "2": {
-        System.out.println("Here is your list of phone numbers:");
-        for (int i = 0; i < limit; i++) {
-          System.out.println((i + 1) + "\t" + generateNumber());
-        }
-        System.out.println("Happy Calling!");
-        break;
-      }
-      default:
-        System.out.println("You didn't enter a valid choice");
-        System.out.println("Please try again");
-        System.out.println("======================================");
-      }
-    }
+				   if (flag) {
+					   System.out.println("The amount of numbers to generate has been changed to "
+				           + limit);
+				   } else {
+					   System.out.println("You did not give me an integer number.");
+					   System.out.println("The amount of numbers will stay at: " + limit);
+					   System.out.println();
+					   System.out.println("======================================");
+				   }
+				   break;
+			   
+			   case "2":
+				   System.out.println("Here is your list of phone numbers:");
+				   for (int i = 0; i < limit; i++) {
+					   System.out.println((i + 1) + "\t" + generateNumber());
+				   }
+				   System.out.println("Happy Calling!");
+				   break;
+			
+			   default:
+				   System.out.println("You didn't enter a valid choice");
+				   System.out.println("Please try again");
+				   System.out.println("======================================");
+			}
+		}
 
-    System.out.println("Aloha!");
-    scanner.close();
-  }
+		System.out.println("Aloha!");
+		scanner.close();
+	}
 
-  /**
-   * Generates a random phone number with a 3-digit area code, 3-digit central office code,
-   * and a 4-digit line number.
-   * 
-   * @return formatted phone number as a String
-   */
-  public static String generateNumber() {
-    Random random = new Random();
+	/**
+	 * Generates a random phone number with a 3-digit area code, 3-digit central
+	 * office code, and a 4-digit line number.
+	 * 
+	 * @return formatted phone number as a String
+	 */
+	public static String generateNumber() {
+		Random random = new Random();
 
-    int areaCode = random.nextInt(100, 800);
+		int areaCode = random.nextInt(100, 800);
 
-    int middleDigits = random.nextInt(100, 750 + 1);
+		int middleDigits = random.nextInt(100, 750 + 1);
 
-    int lastDigits = random.nextInt(10000);
+		int lastDigits = random.nextInt(10000);
 
-    DecimalFormat fourDigitFormat = new DecimalFormat("0000");
-    DecimalFormat threeDigitFormat = new DecimalFormat("000");
+		DecimalFormat fourDigitFormat = new DecimalFormat("0000");
+		DecimalFormat threeDigitFormat = new DecimalFormat("000");
 
-    String first = threeDigitFormat.format(areaCode);
-    String second = threeDigitFormat.format(middleDigits);
-    String third = fourDigitFormat.format(lastDigits);
-    String phoneNumber = "(" + first + ") " + second + "-" + third;
+		String first = threeDigitFormat.format(areaCode);
+		String second = threeDigitFormat.format(middleDigits);
+		String third = fourDigitFormat.format(lastDigits);
+		String phoneNumber = "(" + first + ") " + second + "-" + third;
 
-    return phoneNumber;
-  }
+		return phoneNumber;
+	}
 }
